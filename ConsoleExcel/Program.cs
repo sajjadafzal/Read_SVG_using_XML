3d23d9a61cead00a6b75355b22f6ed233109b78f
sajjadafzal <sajjadafzal@gmail.com>
2019-12-05 15:39:27 +0500

    Excel File read write testings

---
+++ ConsoleExcel/Program.cs
@@ -0,0 +1,66 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ConsoleExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel Exl = new Excel();
+            //Console.WriteLine("Hello World!");
+            //Exl.CreateNewExcelFile();
+            Exl.ReadExcelFileUsingStream();
+            Console.Read();
+
+
+        }
+
+        
+    }
+
+    class Excel
+    {
+        public void CreateNewExcelFile()
+        {
+            ExcelPackage pck = new ExcelPackage();
+            //var ws = pck.Workbook
+            var ws = pck.Workbook.Worksheets.Add("Sample1");
+            ws.Cells[1, 1].Value = "Sajjad Afzal Khattaka";
+            pck.SaveAs(new FileInfo("D:\\EPPlus.xlsx"));
+        }
+
+        public void ReadExcelFile()
+        {
+            FileInfo fileInfo = new FileInfo("D:\\EPPlus.xlsx");
+            ExcelPackage pck = new ExcelPackage(fileInfo);
+            ExcelWorksheet ws = pck.Workbook.Worksheets[0];
+            Console.WriteLine(ws.Cells[1, 1].Value);
+
+        }
+        public void ReadExcelFileUsingStream()
+        {
+            //FileInfo fileInfo = new FileInfo("D:\\EPPlus.xlsx");
+            string path = "D:\\EPPlus.xlsx";
+
+            var lst = new List<DataTableDTO>();
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                pck.Load(fs);
+                ExcelWorksheet ws = pck.Workbook.Worksheets[0];
+                Console.WriteLine(ws.Cells[1, 1].Value);
+            }
+
+        }
+
+    }
+
+    public class DataTableDTO
+    {
+        public string Column1 { get; set; }
+        public string Column2 { get; set; }
+    }
+}
---
+++ ConsoleExcelNet_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ConsoleExcelNet_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application excel = new Excel.Application();
+
+            excel.Visible = true;
+
+
+            Excel._Workbook wb = excel.Workbooks.Add();
+
+            Excel._Worksheet w = (Excel._Worksheet)excel.Worksheets.Add();
+            w.Name = "MYSheet";
+            w.Cells[1, 1] = "Hello";
+            w.SaveAs("D:\\Test.xlsx");
+            //wb.Save("D:\\Test.xlsx");
+        }
+    }
+}

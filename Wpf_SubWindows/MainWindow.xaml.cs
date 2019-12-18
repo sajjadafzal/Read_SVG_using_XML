ad22e2be7af680b98b6b5fe11ef3b0db430b00c7
sajjadafzal <sajjadafzal@gmail.com>
2019-12-18 16:02:41 +0500

    WIP: SubWindow Data Binding

---
+++ Wpf_SubWindows/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_SubWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WinSettings winSettings;
+        private Settings settings;
+        public MainWindow()
+        {
+            InitializeComponent();
+            settings = new Settings();            
+        }
+
+        private void btn_NewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            if (winSettings == null)
+            {
+                winSettings = new WinSettings(settings);
+                winSettings.Closed += delegate { winSettings = null; };
+                winSettings.Owner = this;
+                winSettings.ShowDialog();
+            }
+            //if (winSettings.IsLoaded)
+            
+        }
+    }
+}
+
+public class Settings : DependencyObject,INotifyPropertyChanged
+{
+    private bool _CB1_Check;
+    public bool CB1_Check 
+    { 
+        get 
+        {
+            return _CB1_Check;
+        } 
+        set 
+        {
+            _CB1_Check = value;
+        }
+    }
+    public Settings()
+    {
+
+    }
+
+    event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+    {
+        add
+        {
+            throw new NotImplementedException();
+        }
+
+        remove
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Wpf_SubWindows/WinSettings.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_SubWindows
+{
+    /// <summary>
+    /// Interaction logic for WinSettings.xaml
+    /// </summary>
+    public partial class WinSettings : Window
+    {
+        Settings mySettings;
+        private bool IsChecked;
+        public WinSettings(Settings settings)
+        {
+            InitializeComponent();
+            mySettings = settings;
+            //IsChecked = mySettings.CheckBoxOneisChecked;
+            //this.DataContext = IsChecked;
+            //this.CB1.IsChecked = mySettings.CheckBoxOneisChecked;
+            //this.CB2.IsChecked = mySettings.CB2;
+            //this.CB1.DataContext = mySettings;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            //this.Topmost = true;
+        }
+
+        private void btn_ShowOwner_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.Owner.ToString());
+        }
+    }
+}

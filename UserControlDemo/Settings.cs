781349a21c0b5bf8bd84bf906c17887332690d86
sajjadafzal <sajjadafzal@gmail.com>
2019-12-31 15:31:23 +0500

    WIP: UserControl DependencyProperty TwoWay Binding

---
+++ UserControlDemo/Settings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace UserControlDemo
+{
+    public class SliderControlSettings : Settings
+    {
+        int _value;
+        public string Label { get; set; }
+        public int Minimun { get; set; } = 0;
+        public int Maximum { get; set; } = 100;
+        public int PropertyValue
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                if ((value <= Maximum) && (value >= Minimun))
+                {
+                    _value = value;
+                    RaisePropertyChanged("PropertyValue");
+                }
+            }
+        }
+
+        public SliderControlSettings(string label,int minimum, int maximum, int value)
+        {
+            Label = label;
+            Minimun = minimum;
+            Maximum = maximum;
+            PropertyValue = value;
+        }
+    }
+    public class Settings : INotifyPropertyChanged
+    {
+        public PropertyChangedEventHandler propertyChanged;
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                propertyChanged += value;
+                //throw new NotImplementedException();
+            }
+
+            remove
+            {
+                propertyChanged -= value;
+                //throw new NotImplementedException();
+            }
+        }
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (this.propertyChanged != null)
+            {
+                this.propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ UserControlDemo/UserControls/SliderControl.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlDemo.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SliderControl.xaml
+    /// </summary>
+    public partial class SliderControl : UserControl, INotifyPropertyChanged
+    {
+        public SliderControl()
+        {
+            InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;            
+        }
+
+        public int PropertyValue
+        {
+            get { return (int)GetValue(PropertyValueProperty); }
+            set { SetValue(PropertyValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PropertyValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PropertyValueProperty =
+            DependencyProperty.Register("PropertyValue", typeof(int), typeof(SliderControl), new PropertyMetadata(OnPropertyValueChanged));
+
+        static void OnPropertyValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            (obj as SliderControl).OnPropertyValueChanged(e);
+        }
+
+        private void  OnPropertyValueChanged(DependencyPropertyChangedEventArgs e)
+        {
+            int PropertyNewValue = (int)e.NewValue;
+            SliderControlSlider.Value = PropertyNewValue;            
+        }
+
+        public int Minimum
+        {
+            get { return (int)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Minimum.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(int), typeof(SliderControl), new PropertyMetadata(0));
+
+
+
+        public int Maximum
+        {
+            get { return (int)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Maximum.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(int), typeof(SliderControl), new PropertyMetadata(0));
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(SliderControl));
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void SetValueDp(DependencyProperty property, object value, [System.Runtime.CompilerServices.CallerMemberName] string p = null)
+        {
+            SetValue(property, value);
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+
+    
+}
